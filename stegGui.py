3d9d16e76fc021cd7ecbd28d93cd1954ed9cfd63
David <deldel@suddenlink.net>
2016-07-15 15:44:17 -0500

    This is the completed version of the GUI required to use the steganography program. Unlike previous versions, steganography,py no longer can run on its own. From now own, run the stegGui.py program to perform encoding or decoding on an image.

--- stegGui.py
+++ stegGui.py
@@ -1,90 +1,87 @@
+"""
+The following is a GUI comprised of a
+series of menus that enable to the user to
+use my steganography program. With the addition
+of this GUI, the previous version of the
+steganography.py program has been changed to
+no longer work on its own. Instead, the
+functions that previously existed under the
+older version have been changed to blend with
+this GUI, which now serves as the main function
+for running my program. For more information
+on how the steganography process works, see
+the steganography.py file.
+
+Author: David Letourneau, 7/13/2016
+"""
 
 from tkinter import *
 from tkinter.filedialog import askopenfilename, asksaveasfilename
-
-# def callback():
-#     name = askopenfilename()
-#     print(name)
-#
-# # For the actual object, add this method as a "ENCODE NOW" type button method
-# def getInput():
-#     input = text.get("1.0", "end-1c") # end is the end of the text box, -1 is 1 character less, to remove new line
-#     print(input)
-#
-# errmsg = 'Error!'
-# Button(text='File Open', command=getInput).pack(fill=X)
-#
-# text = Text(height=6, width=75, wrap=WORD)
-# text.pack()
-
-
-
-# This seems to be needed to get it to perform.
-#mainloop()
-
-
-# Try to make it in class format below
+from steganography import encodeGUI as encode, decodeGUI as decode
 
 class startMenu:
     """A class containing GUI functons for use
     in my steganography program. The start
     menu calls the other menus depending on
-    the user's request."""
+    the user's request(Either encode or
+    decode.)"""
     def __init__(self, master):
         # Variables for this menu
         self.mode = IntVar()                # Encode or Decode mode
-        # self.saveName = "dummy save path"   # file name of new image
         self.master = master                # name of the tk instance
         self.frame = Frame(self.master)     # frame of this window
-        self.child = None                   # Initially there is no child
+        self.child = None                   # Child windows created
+
         # Button variables - File explorer and mode boxes respectively
-        #self.fileButton = Button(self.frame, text='Class button', command=self.call).pack(fill=X)
-        self.EncodeBox = Radiobutton(self.frame, text='Encode', variable=self.mode, value=0).pack(anchor=W)
-        self.DecodeBox = Radiobutton(self.frame, text='Decode', variable=self.mode, value=1).pack(anchor=W)
-        #self.saveButton = Button(self.frame, text='Save filename', command=self.save).pack(fill=X)
-        self.proceedButton = Button(self.frame, text='Next', command=self.openMenu).pack(fill=X)
-        # personal use button
-        self.test = Button(self.frame, text='Print mode', command=self.pmode).pack(fill=X)
+        self.EncodeBox = Radiobutton(self.frame, text='Encode', variable=self.mode, value=0).pack()
+        self.DecodeBox = Radiobutton(self.frame, text='Decode', variable=self.mode, value=1).pack()
+        self.proceedButton = Button(self.frame, text='Next', command=self.openMenu)
+        self.proceedButton.pack(side=LEFT, pady=5, padx=25, ipadx=25)
+        self.quitButton = Button(self.frame, text='Quit', command=self.close)
+        self.quitButton.pack(side=LEFT, pady=5, padx=25, ipadx=25)
         self.frame.pack()
+
+    def close(self):
+        # Closes the current window.
+        self.master.destroy()
 
     def openMenu(self):
         """This method is called
         when the user clicks the PROCEED
-        button, and creates a child window
+        button, which creates a child window
         of either the Encode or Decode
         menus depending on the user's
         selection."""
         self.child = Toplevel(self.master)
+
         # Create a child window depending on the mode
         if self.mode.get() == 0:
             self.app = EncodeMenu(self.child)
         else:
             self.app = DecodeMenu(self.child)
 
-    def pmode(self):
-        print(self.mode.get())
-        print(self.saveName)
-
-    def save(self):
-        self.saveName = asksaveasfilename()
-
 class EncodeMenu:
     """ The encode menu is the menu
     that appears when the user request
     the ability to encode a message into
-    an image.
-    """
+    an image. It makes use of the
+    encodeGUI function from the file
+    steganography.py"""
     def __init__(self, master):
-        # Set up the Tk box
+        # Set up the Tkinter box
         self.master = master
         master.title("Image Encoder")
         self.frame = Frame(self.master)
-        # Variables required for this menu
-        self.filepath = None
-        self.userInput = Text(self.frame, height=8, width=45, wrap=WORD, borderwidth=3).pack()
-        self.userPath = Text(self.frame, height=1, width=45, borderwidth=3, state=DISABLED)
         self.label = Label(master, text="Insert Message Below").pack()
+
+        # Variables required for this menu
+        self.filepath = ""
+        self.userInput = Text(self.frame, height=8, width=45, wrap=WORD, borderwidth=3)
+        self.userPath = Text(self.frame, height=1, width=45, borderwidth=3, state=DISABLED)
+        self.savePath = "Initially save path is empty."
+        self.userInput.pack()
         self.userPath.pack()
+
         # Buttons
         self.quit = Button(self.frame, text="Cancel", command=self.close)
         self.quit.pack(side=RIGHT, ipadx=10, padx=7, expand=0)
@@ -100,7 +97,86 @@ class EncodeMenu:
         use of the filebrowser, then
         displays the file path in the
         text box."""
-        self.filepath = askopenfilename()
+        self.filepath = askopenfilename(filetypes=[
+            ('JPG files', '*.jpg'),
+            ('PNG files', '*.png'),
+            ('BMP files', '*.bmp'),
+            ('JPEG files', '*.jpeg')
+        ])
+
+        """The following changes the text
+        box to allow insertion of text,
+        places the filepath inside it, and
+        resets it to disable editing so the
+        user can't interfere with the filepath."""
+        self.userPath.configure(state=NORMAL)
+        self.userPath.delete("1.0", END)
+        self.userPath.insert(INSERT, self.filepath)
+        self.userPath.configure(state=DISABLED)
+
+    def close(self):
+        # Closes the current window.
+        self.master.destroy()
+
+    def enc(self):
+        """Method for encoding the
+        user's message into an image."""
+
+        self.savePath = asksaveasfilename(filetypes=[
+            ('BMP file', '*.bmp'),
+            ('PNG file', '*.png')
+        ])
+
+        # Make sure the user entered a name. if not, do nothing
+        if len(self.filepath) == 0 or len(self.savePath) == 0:
+            return
+
+        """The range 1.0 - end-1c starts at the beginning
+        of the string and extends until the end, remove the
+        new line character added by the Text widget.
+        The encode function is imported from steganography.py"""
+        encode(self.userInput.get("1.0",'end-1c'), self.filepath, self.savePath)
+
+
+class DecodeMenu:
+    """ The decode menu appears
+    when the user requests the
+    ability to decode a message
+    from a previously encoded image."""
+    def __init__(self, master):
+        self.master = master
+        master.title("Image Decoder")
+        self.frame = Frame(self.master)
+        self.label = Label(master, text="Decoded message will appear below")
+        self.label.pack()
+        self.frame.pack()
+
+        # variables
+        self.filepath = None
+        self.message = Text(self.frame, height=8, width=45, wrap=WORD, borderwidth=3, state=DISABLED)
+        self.userPath = Text(self.frame, height=1, width=45, borderwidth=3, state=DISABLED)
+        self.message.pack()
+        self.userPath.pack()
+
+        # Buttons
+        self.quit = Button(self.frame, text="Cancel", command=self.close)
+        self.quit.pack(side=RIGHT, ipadx=10, padx=7, expand=0)
+        self.getFile = Button(self.frame, text="Select Image", command=self.getFilePath)
+        self.getFile.pack(side=LEFT, ipadx=10, padx=7, expand=0)
+        self.encode = Button(self.frame, text="Decode", command=self.dec)
+        self.encode.pack(side=LEFT, ipadx=10, expand=0)
+        self.frame.pack()
+
+    def getFilePath(self):
+        """This method allows the user
+        to select an image through
+        use of the filebrowser, then
+        displays the file path in the
+        text box."""
+        self.filepath = askopenfilename(filetypes=[
+            ('PNG files', '*.png'),
+            ('BMP files', '*.bmp'),
+        ])
 
         # Change the text box to be editable
         self.userPath.configure(state=NORMAL)
@@ -114,33 +190,33 @@ class EncodeMenu:
     def close(self):
         self.master.destroy()
 
-    def testF(self):
-        print(self.filepath)
+    def dec(self):
+        """The dec method reads from an image
+        previously encoded with a message and
+        prints out the hidden text to the screen.
+        As with the enc method, the decodeGUI
+        function is imported from the file
+        steganography.py, which contains more
+        documentation for the function."""
+
+        # Make sure the user entered a file to decode. if not, do nothing
+        if self.filepath != None and self.filepath != "":
+            message = decode(self.filepath)
+
+            """The following changes the text
+            box to allow insertion of text,
+            places the filepath inside it, and
+            resets it to disable editing so the
+            user can't interfere with the filepath."""
+            self.message.configure(state=NORMAL)
+            self.message.delete("1.0", END)
+            self.message.insert(INSERT, message)
+            self.message.configure(state=DISABLED)
 
 
-    def enc(self):
-        """Method for encoding the
-        user's message into an image."""
-        print("wow. Wink")
-
-
-class DecodeMenu:
-    """ The decode menu appears
-    when the user requests the
-    ability to decode a message
-    from a previously encoded image.
-    """
-    def __init__(self, master):
-        self.master = master
-        master.title("Image Decoder")
-        self.frame = Frame(self.master)
-        self.quit = Button(self.frame, text="Cancel", command=self.close).pack()
-        self.frame.pack()
-
-    def close(self):
-        self.master.destroy()
-# Object will likely need to be instantiated from Steg
+# Instantiate the GUI object and run the program
 root = Tk()
+root.geometry("235x100")
 root.title("Steganographer")
 g = startMenu(root)
 
--- steganography.py
+++ steganography.py
@@ -4,7 +4,12 @@ and transcribes the message into a new version of the image
 using the parity of the RGB values within the image. The user
 can request to encode a message into an image given an
 appropriate path, or decode a message that has already
-been encoded into an existing image.
+been encoded into an existing image. Please note that
+this program was written in a version of python 3, and
+therefore requires the use of the Pillow library to work,
+since the PIL library is not officially supported in python
+3. Documentation and downloads for the community supported
+pillow library can be found online.
 
 Author: David Letourneau, 6/29/2016
 """
@@ -13,60 +18,94 @@ from PIL import Image
 
 # Function definitions
 
-# Function for converting a text string into binary
 def textToBinary(text):
-   binary = ''.join(format(ord(x), 'b').zfill(8) for x in text)
-   return binary
+    """Function for converting a
+    text string into binary"""
+    binary = ''.join(format(ord(x), 'b').zfill(8) for x in text)
+    return binary
 
-# Function for encoding the message into the image
-def encode(bitString, image, width, height):
-    pixels = image.load()           # List of the pixels in the image
+
+def encodeGUI(bitString, imagePath, savePath):
+    """This function is used for
+    encoding a message into an image
+    and is used by the stegGui.py file
+    to perform encoding through the GUI"""
+    image = Image.open(imagePath)
+    image = image.convert("RGB")
+    width, height = image.size
+
+    bitString = bitConversions(bitString, width, height)
+
+    imageCpy = image                # create a copy of the image for modifying
+    pixels = imageCpy.load()        # List of the pixels in the image
     b = 0                           # to iterate through bitString
     length = len(bitString)         # length of message
 
     for w in range(0, width):
         for h in range(0, height):
-
             # The following is performed for the R, G, B values per pixel
             for x in range(0, 3):
                 # Check to see if we've finished encoding the message.
                 if b == length:
-                    badSaveRequest = True
-                    output = input(
-                        "Where would you like this image to be saved? Enter a path ending with the desired file "
-                        "name and format(png or bmp): ")
-                    while badSaveRequest:
-                        try:
-                            image.save(output)
-                            print("Your image has been saved in the location ", output)
-                            badSaveRequest = False
-                        except FileNotFoundError:
-                            output = input("The requested path does not exist. Please try again: ")
+                    # Save the image in the location specified(savePath)
+                    imageCpy.save(savePath)
                     return
 
+                # Since we haven't finished, we can begin encoding
                 color = pixels[w, h]  # get a tuple of the RGB value at a given pixel.
                 if bitString[b] == "1":
                     if color[x] % 2 == 0:
                         # our color has an even value, add one to make it odd
                         rgbList = list(color)
-                        rgbList[x] += 1 # + 1
+                        rgbList[x] += 1
                         pixels[w, h] = tuple(rgbList)  # set the new pixel values
                 else:
                     if color[x] % 2 == 1:
                         rgbList = list(color)
-                        rgbList[x] -= 1 # - 1
+                        rgbList[x] -= 1
                         pixels[w, h] = tuple(rgbList)  # set pixel values
                 b += 1
 
 
-# Function for decoding messages out of an image that has been encoded
-def decode(image, width, height):
+def bitConversions(text, width, height):
+    """This function takes a string in
+    ASCII, and performs all the neccesary
+    computation to convert it into a usable
+    binary string, including appending the
+    null termination byte so the decode
+    function knows where to stop reading."""
+    bitString = textToBinary(text)
+
+    # Check if the message is too long to fit inside the image
+    totalBits = len(bitString)          # total number of bits in the message
+    totalPixels = width * height        # total numbers of pixels in the image
+    bitMax = totalPixels * 3            # total number of bits that can fit in the image(3 per pixel)
+
+    # The new bit string is a sub string that includes as many characters that can fit
+    # in the image, plus the null termination bits used in decoding the message.
+    if (totalBits + 9) > bitMax:
+        print("Message is too long and will be truncated to fit")
+        bitString = bitString[0:bitMax - 9]
+
+    # append on the null termination byte to the end of the binString (00000000)
+    bitString += "00000000"
+    return bitString
+
+def decodeGUI(imagePath):
+    """This function is used for
+    decoding the message hidden inside a
+    previously encoded image. It is used by
+    the GUI program."""
+    image = Image.open(imagePath)
+    image = image.convert("RGB")
+    width, height = image.size
+
     message = ""
-    zeroCounter = 0              # This variable is used to detect the null termination byte
+    zeroCounter = 0                 # This variable is used to detect the null termination byte
     pixels = image.load()
     for w in range(0, width):
         for h in range(0, height):
-            color = pixels[w, h]  # get a tuple of the RGB values at a given pixel.
+            color = pixels[w, h]    # get a tuple of the RGB values at a given pixel.
             for x in range(0, 3):
                 bit = color[x]
                 if zeroCounter == 8:
@@ -75,9 +114,10 @@ def decode(image, width, height):
                     encounters the null termination byte, which signifies that
                     it has read the entire message. First, check to make sure
                     the total length of the message is a multiple of 8. If it isn't,
-                    some of the bits might have been cut off before reaching the null termination
-                    character. In this case, we pad with 0's on the right to reach
-                    a multiple of 8, and then convert the bit string back into ASCII.
+                    some of the bits might have been cut off before reaching the null
+                    termination character. In this case, we pad with 0's on the right
+                    to reach a multiple of 8, and then convert the bit string back into
+                    ASCII.
                     """
                     pad = len(message) % 8
                     if pad != 0:
@@ -92,80 +132,16 @@ def decode(image, width, height):
                 else:
                     zeroCounter = 0
 
-# Function to convert a binary string into an ASCII string
 def convertFromBinary(string):
-    # Convert the bit string into an int format
-    # number = int(string, 2)
+    """This is a helper function
+    designed to convert a string from
+    binary into the corresponding ASCII
+    string"""
 
     # Convert the int into the corresponding ASCII symbols and return
-    # return number.to_bytes((number.bit_length() + 7) // 8, 'big').decode()
     return ''.join(chr(int(string[i:i + 8], 2)) for i in range(0, len(string), 8))
 
-
 # End of function definitions
-
-version = "V_2.3"
-
-print("Steganography creator version", version, "\n")
-
-# Ask the user if they wish to Encode a message or Decode from an image
-request = ""
-while request.lower() != "q" and request.lower() != "e" and request.lower() != "d":
-    request = input("Would you like to encode a message onto an image or decode from an existing file? Please enter "
-                "E to encode, D to decode, or Q to quit: ")
-
-# Ask the user for the path to the image requested
-badRequest = True
-image = None
-while badRequest:
-    imagePath = input("Please enter the path to the image you would like to use or Q to quit: ")
-    if imagePath.lower() == "q":
-        # User has request to quit the program
-        exit()
-    try:
-        # Attempt to use the path suggested & break out of while loop
-        image = Image.open(imagePath)
-        image = image.convert("RGB")
-        width, height = image.size  # size properties of the image
-        badRequest = False
-    except FileNotFoundError:
-        # User has entered a request for a file that cannot be found
-        print("The file requested could not be found or does not exist")
-
-# if the user selected encode, there is initial set up required below
-if request.lower() == 'e':
-    # Get the text the user wishes to embed within the image
-    text = input("Please enter the text you wish to place within the image: ")
-    binString = textToBinary(text)  # convert user's string to binary
-    imageCpy = image                # create a copy of the image for modifying
-
-    # Check if the message is too long to fit inside the image
-    totalBits = len(binString)      # total number of bits in the message
-    totalPixels = width * height    # total numbers of pixels in the image
-    bitMax = totalPixels * 3        # total number of bits that can fit in the image(3 per pixel)
-
-
-
-    # The new bit string is a sub string that includes as many characters that can fit
-    # in the image, plus the null termination bits used in decoding the message.
-    if (totalBits + 9) > bitMax:
-        print("Message is too long and will be truncated to fit")
-        binString = binString[0:bitMax - 9]
-
-    # append on the null termination byte to the end of the binString (00000000)
-    binString += "00000000"
-
-    encode(binString, imageCpy, width, height)
-else:
-    # this block is used when the user requests decode
-    width, height = image.size
-    image = Image.open(imagePath)
-    message = decode(image, width, height)
-    print("Message is:", message)
-
-
-
-
 
 
 
